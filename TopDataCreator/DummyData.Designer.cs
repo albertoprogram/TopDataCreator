3954fa3171d4341b3842f2119c982bceff1a7c49
Alberto Chirinos <developernet@outlook.com>
2022-07-25 20:17:02 -0500

    DummyData form creation.

---
+++ TopDataCreator/DummyData.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace TopDataCreator
+{
+    partial class DummyData
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblConnection = new System.Windows.Forms.Label();
+            this.lblDatabaseServer = new System.Windows.Forms.Label();
+            this.txtDatabaseServer = new System.Windows.Forms.TextBox();
+            this.lblSQLServer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblConnection
+            // 
+            this.lblConnection.AutoSize = true;
+            this.lblConnection.Location = new System.Drawing.Point(31, 27);
+            this.lblConnection.Name = "lblConnection";
+            this.lblConnection.Size = new System.Drawing.Size(84, 20);
+            this.lblConnection.TabIndex = 0;
+            this.lblConnection.Text = "Connection";
+            // 
+            // lblDatabaseServer
+            // 
+            this.lblDatabaseServer.AutoSize = true;
+            this.lblDatabaseServer.Location = new System.Drawing.Point(31, 75);
+            this.lblDatabaseServer.Name = "lblDatabaseServer";
+            this.lblDatabaseServer.Size = new System.Drawing.Size(113, 20);
+            this.lblDatabaseServer.TabIndex = 1;
+            this.lblDatabaseServer.Text = "DatabaseServer";
+            // 
+            // txtDatabaseServer
+            // 
+            this.txtDatabaseServer.Location = new System.Drawing.Point(150, 68);
+            this.txtDatabaseServer.Name = "txtDatabaseServer";
+            this.txtDatabaseServer.Size = new System.Drawing.Size(236, 27);
+            this.txtDatabaseServer.TabIndex = 2;
+            // 
+            // lblSQLServer
+            // 
+            this.lblSQLServer.AutoSize = true;
+            this.lblSQLServer.Location = new System.Drawing.Point(150, 27);
+            this.lblSQLServer.Name = "lblSQLServer";
+            this.lblSQLServer.Size = new System.Drawing.Size(80, 20);
+            this.lblSQLServer.TabIndex = 3;
+            this.lblSQLServer.Text = "SQL Server";
+            // 
+            // DummyData
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblSQLServer);
+            this.Controls.Add(this.txtDatabaseServer);
+            this.Controls.Add(this.lblDatabaseServer);
+            this.Controls.Add(this.lblConnection);
+            this.Name = "DummyData";
+            this.Text = "DummyData";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label lblConnection;
+        private Label lblDatabaseServer;
+        private TextBox txtDatabaseServer;
+        private Label lblSQLServer;
+    }
+}
